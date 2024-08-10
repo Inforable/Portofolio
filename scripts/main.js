8a3b24201236add530e309087e80d8eb1f400333
Inforable <hasri.fayadh@gmail.com>
2024-08-10 22:45:47 +0700

    Penambahan fitur di responsive dan penambahan file resume

--- scripts/main.js
+++ scripts/main.js
@@ -1,6 +1,19 @@
+// Toggle and Responsive
+const navSlide = () => {
+    const burger = document.querySelector(".burger")
+    const navLists = document.querySelector("nav")
+
+    burger.addEventListener("click", () => {
+        navLists.classList.toggle("nav-active")
+        burger.classList.toggle("toggle-burger")
+    })
+}
+
+navSlide();
+
 // Clean form before unload
 window.onbeforeunload = () => { 
     for (const form of document.getElementsByTagName("form")) { // iterate tiap form yang ada biar direset
         form.reset();
     }
-}
+};
